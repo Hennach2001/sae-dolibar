0fea0b8ba666497b9a148e12b318d4b48dcdbb1a
Waped <aliounethiaw100@gmail.com>
2024-11-17 14:54:44 +0100

    Changes to be committed: 	modified:   test/docker-compose.yml

--- test/scripts/docker-init.php
+++ test/scripts/docker-init.php
@@ -3,8 +3,47 @@
 require_once '../htdocs/master.inc.php';
 require_once DOL_DOCUMENT_ROOT.'/core/lib/admin.lib.php';
 
-$mods = ['User', 'Fournisseur', 'Societe', 'Propale', 'Commande'];
+$mods = ['User', 'Import', 'Societe'];
 foreach ($mods as $mod) {
         activateModule('mod'.$mod);
+};
+
+printf("OK\n");
+
+if (!empty(getenv('DOLI_COMPANY_COUNTRYCODE'))) {
+  require_once DOL_DOCUMENT_ROOT.'/core/lib/company.lib.php';
+  require_once DOL_DOCUMENT_ROOT.'/core/class/ccountry.class.php';
+  $countryCode = getenv('DOLI_COMPANY_COUNTRYCODE');
+  $country = new Ccountry($db);
+  $res = $country->fetch(0,$countryCode);
+  if ($res > 0 ) {
+    $s = $country->id.':'.$country->code.':'.$country->label;
+    dolibarr_set_const($db, "MAIN_INFO_SOCIETE_COUNTRY", $s, 'chaine', 0, '', $conf->entity);
+    printf('Configuring for country : '.$s."\n");
+    activateModulesRequiredByCountry($country->code);
+  } else {
+    printf('Unable to find country '.$countryCode."\n");
+  }
 }
-?>
+
+if (!empty(getenv('DOLI_COMPANY_NAME'))) {
+  $compname = getenv('DOLI_COMPANY_NAME');
+  dolibarr_set_const($db, "MAIN_INFO_SOCIETE_NOM", $compname, 'chaine', 0, '', $conf->entity);
+}
+
+if (!empty(getenv('DOLI_ENABLE_MODULES'))) {
+  $mods = explode(',', getenv('DOLI_ENABLE_MODULES'));
+  foreach ($mods as $mod) {
+    printf("Activating module ".$mod." ...");
+    try { 
+      $res = activateModule('mod' . $mod);
+      if ($res < 0) { 
+        print(" FAILED. Unable to load module. Be sure to check the case\n");
+      } else {
+        printf(" OK\n");
+      }
+    } catch (Throwable $t) {
+      print(" FAILED. Unable to load module. Be sure to check the case\n");
+    }
+  }
+}
